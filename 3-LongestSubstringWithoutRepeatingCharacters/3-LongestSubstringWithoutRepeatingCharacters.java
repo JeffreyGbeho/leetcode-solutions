c3619efa354f97fa4335855f3d5a15abc7fa0824
Jeffrey Gbeho <jeff.gbeho@gmail.com>
2024-02-13 23:38:55 +0100

    Update file 13/02/2024 23:38:55

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,18 +1,18 @@
+        } 
+        return -1;
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if ((m + n) % 2 == 0) {
+            return nums2[p2++];
+        } else if (p2 < nums2.length) {
+            return nums1[p1++];
+        } else if (p1 < nums1.length) {
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+    private int getMin(int[] nums1, int[] nums2) {
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int index = 0, res = 0, count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                res = Math.max(res, count);
-                map = new HashMap<>();
-                count = 0;
-                index++;
-                i = index;
-            }
-                
-            map.put(s.charAt(i), 0);
-            count++;
-        }
-                
-"abcabcbb"
+    private int p1 = 0, p2 = 0;
+
+    
+[1,3]
