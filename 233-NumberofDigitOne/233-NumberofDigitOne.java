643ddb132c0dc95e441106aa10ceefb453b97ce2
Jeffrey Gbeho <jeff.gbeho@gmail.com>
2024-02-14 00:19:35 +0100

    Create file

---
+++ 233-NumberofDigitOne/233-NumberofDigitOne.java
@@ -0,0 +1,17 @@
+                // Its result is multiplied with sign on top of stack
+                // as stack.pop() is the sign before the parenthesis
+                result *= stack.pop();
+
+                // Then add to the next operand on the top.
+                // as stack.pop() is the result calculated before this parenthesis
+                // (operand on stack) + (sign on stack * (result from parenthesis))
+                result += stack.pop();
+
+                // Reset the operand
+                operand = 0;
+            }
+        }
+        return result + (sign * operand);
+    }
+}
+"1 + 1"
