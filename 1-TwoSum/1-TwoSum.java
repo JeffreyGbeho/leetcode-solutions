6c1d096f1658d18129c26fd3a1e9783d263f248e
Jeffrey Gbeho <jeff.gbeho@gmail.com>
2024-02-13 23:36:15 +0100

    Create file

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,19 @@
+        Hashtable<Integer, Integer> hashtable = new Hashtable<Integer, Integer>();
+    public int[] twoSum(int[] nums, int target) {
+    // One-pass Hash Table O(n)
+
+    // }
+    //     return null;
+
+    //     }
+    //         }
+    //             }
+    //                 return new int[] { i, j };
+    //             if (target - nums[j] == nums[i]) {
+    //         for(int j = i+1; j < nums.length; j++) {
+    //     for(int i = 0; i < nums.length; i++) {
+    // public int[] twoSum(int[] nums, int target) {
+    // Brut force solution O(n²) GOOD
+class Solution {
+
+[2,7,11,15]
