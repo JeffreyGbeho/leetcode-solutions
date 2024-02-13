055c88c143c774f328d19f4c4ebe46dca349027e
Jeffrey Gbeho <jeff.gbeho@gmail.com>
2024-02-13 23:39:19 +0100

    Create file

---
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int p1 = 0, p2 = 0;
+
+    private int getMin(int[] nums1, int[] nums2) {
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.length) {
+            return nums1[p1++];
+        } else if (p2 < nums2.length) {
+            return nums2[p2++];
+        } 
+        return -1;
+    }
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if ((m + n) % 2 == 0) {
+[1,3]
