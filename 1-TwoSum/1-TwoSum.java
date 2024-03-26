c19090ec2f974243bdf1735c755dff764e7a22b8
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2024-03-26 16:16:09 +0100

    Update file 26/03/2024 16:16:09

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,19 +1,19 @@
-        Hashtable<Integer, Integer> hashtable = new Hashtable<Integer, Integer>();
-    public int[] twoSum(int[] nums, int target) {
-    // One-pass Hash Table O(n)
-
-    // }
     //     return null;
+    // }
 
-    //     }
-    //         }
-    //             }
-    //                 return new int[] { i, j };
-    //             if (target - nums[j] == nums[i]) {
-    //         for(int j = i+1; j < nums.length; j++) {
-    //     for(int i = 0; i < nums.length; i++) {
-    // public int[] twoSum(int[] nums, int target) {
-    // Brut force solution O(n²) GOOD
-class Solution {
+    // One-pass Hash Table O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer, Integer> hashtable = new Hashtable<Integer, Integer>();
 
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.get(nums[i]) != null) {
+                return new int[] { hashtable.get(nums[i]), i };
+            }
+            
+            hashtable.put(target - nums[i], i);
+        }
+
+        return null;
+    }
+}
 [2,7,11,15]
