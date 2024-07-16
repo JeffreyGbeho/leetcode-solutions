a8e9b6de84272d4602dfcbd3c3ff13148dd8bbed
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2024-07-16 10:47:22 +0200

    Create file

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            var temp = map.get(target - nums[i]);
+            if (temp != null) {
+                int[] res = {temp, i};
+                return res;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
