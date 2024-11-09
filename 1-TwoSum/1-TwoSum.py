eecdb91a9252f9d930d494663103bbafc27c78ab
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2024-11-09 11:48:46 +0100

    Update file 09/11/2024 11:48:46

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,7 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i, num in enumerate(nums):
-            f = target - num
-            for j, n in enumerate(nums):
-                if n == f and i != j:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
                     return [i, j]
+        return []  # No solution found
