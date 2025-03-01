5f5f071c3707365e2c9f0705c7339a31ac165e69
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-03-01 11:12:42 +0100

    File updated at 01/03/2025 11:12:42

--- 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
+++ 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
@@ -1,6 +1,2 @@
-// Last updated: 01/03/2025 11:05:01
-def fib(pos, last1, last2):
-   skip = fib(pos + 1, last1, last2)
-   if arr[pos] == arr[last1] + arr[last2]:
-       take = 1 + fib(pos + 1, pos, last1)
-   return max(take, skip)
+// Last updated: 01/03/2025 11:12:42
+x1, x2, ..., xn
