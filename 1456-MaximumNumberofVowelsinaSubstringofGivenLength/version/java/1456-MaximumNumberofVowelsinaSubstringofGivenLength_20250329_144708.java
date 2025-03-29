a45170078e6fed8dcdb98aa58ce9d7d004da2f4d
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-03-29 14:47:08 +0100

    Create file

---
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/version/java/1456-MaximumNumberofVowelsinaSubstringofGivenLength_20250329_144708.java
@@ -0,0 +1,26 @@
+// Last updated: 29/03/2025 14:47:08
+/*
+ * O(n)
+ * Sliding window technique with arrayList to detect vowels
+*/
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        int res = 0;
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += vowels.contains(s.charAt(i)) ? 1 : 0;
+        } 
+        res = Math.max(res, sum);
+
+        for (int i = k; i < s.length(); i++) {
+            sum -= vowels.contains(s.charAt(i-k)) ? 1 : 0;
+            sum += vowels.contains(s.charAt(i)) ? 1 : 0;
+            res = Math.max(res, sum);
+        }
+
+        return res;
+    }
+}
