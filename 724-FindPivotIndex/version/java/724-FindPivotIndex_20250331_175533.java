13942797bc3da109a4dd4243667b7c6fd668bc9a
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-03-31 17:55:32 +0200

    Create file

---
+++ 724-FindPivotIndex/version/java/724-FindPivotIndex_20250331_175533.java
@@ -0,0 +1,20 @@
+// Last updated: 31/03/2025 17:55:33
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            int temp = map.get(key);
+            map.put(key, -1);
+            if (map.containsValue(temp)) {
+                return false;
+            }
+        }
+
+         return true;
+    }
+}
