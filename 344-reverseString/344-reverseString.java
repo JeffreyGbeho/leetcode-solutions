833f6e19ec202414a6c686fc34c56939ce51cc08
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 19:19:44 +0200

    Create file

---
+++ 344-reverseString/344-reverseString.java
@@ -0,0 +1,16 @@
+// Last updated: 18/05/2025 19:19:44
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            char temp = s[right];
+            s[right] = s[left];
+            s[left] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+}
