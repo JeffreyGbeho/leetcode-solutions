62b88f011bec2e478b2f029c6c87004bd2cafbd4
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 11:26:55 +0200

    Create file

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,20 @@
+// Last updated: 18/05/2025 11:26:54
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int l = candies.length; 
+        Boolean[] res = new Boolean[l];
+        
+        for (int i = 0; i < l; i++) {
+            res[i] = true;
+            int temp = candies[i] + extraCandies;
+
+            for (int candy : candies) {
+                if (temp < candy) {
+                    res[i] = false;
+                }
+            }
+        }
+
+        return Arrays.asList(res);
+    }
+}
