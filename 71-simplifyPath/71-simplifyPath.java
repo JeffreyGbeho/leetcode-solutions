2f38013b86eccb882bde8b43fb6d954898655e29
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 19:19:50 +0200

    Create file

---
+++ 71-simplifyPath/71-simplifyPath.java
@@ -0,0 +1,27 @@
+// Last updated: 18/05/2025 19:19:50
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s = new Stack<String>();
+        String[] items = path.split("/");
+
+        for (String str : items) {
+            if (str.equals(".") || str.isEmpty()) {
+                continue;
+            } else if (str.equals("..")) {
+                if (!s.isEmpty()) {
+                    s.pop();
+                }
+            } else {
+                s.add(str);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (String dir : s) {
+            res.append("/");
+            res.append(dir);
+        }
+
+        return res.length() > 0 ? res.toString() : "/";
+    }
+}
