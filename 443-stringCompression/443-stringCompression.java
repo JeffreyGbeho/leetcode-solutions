3dbb99c449084cae99b39c68967c5a9d0e641088
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 19:19:41 +0200

    Create file

---
+++ 443-stringCompression/443-stringCompression.java
@@ -0,0 +1,30 @@
+// Last updated: 18/05/2025 19:19:41
+class Solution {
+    public int compress(char[] chars) {
+        char temp = chars[0];
+        int count = 0;
+        String res = "";
+        int l = chars.length;
+        
+        for (int i = 0; i < l; i++) {
+            if (temp != chars[i]) {
+                res += temp;
+                res += count > 1 ? count : "";
+                temp = chars[i];
+
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        res += temp;
+        res += count > 1 ? count : "";
+
+        for (int i = 0; i < res.length(); i++) {
+            chars[i] = res.charAt(i);
+        }
+
+        return res.toCharArray().length;
+    }
+}
