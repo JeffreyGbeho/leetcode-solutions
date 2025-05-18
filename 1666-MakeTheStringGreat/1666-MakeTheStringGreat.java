d4c856dc705ede4560e6d0a3801e806009bcc320
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 19:31:55 +0200

    Create file

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.java
@@ -0,0 +1,24 @@
+// Last updated: 18/05/2025 19:31:56
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        char[] chars = s.toCharArray();
+        
+        for (Character c : chars) {
+            if (stack.size() == 0) {
+                stack.add(c);
+            } else if (Character.compare(Character.toLowerCase(c), Character.toLowerCase(stack.lastElement())) == 0 && !c.equals(stack.lastElement())) {
+                stack.pop();
+            } else {
+                stack.add(c);
+            }
+        }
+        
+        StringBuilder res = new StringBuilder();
+        for(Character c : stack) {
+            res.append(c);
+        }
+        
+        return res.toString();
+    }
+}
