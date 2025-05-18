33be88959c529d475f4204abc6d4cd12b3e82d0c
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-18 23:07:16 +0200

    Create file

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.java
@@ -0,0 +1,38 @@
+// Last updated: 18/05/2025 23:07:17
+class Solution {
+    public String reverseVowels(String s) {
+        HashMap<Character, Character> dict = new HashMap<Character, Character>();
+        dict.put('a', 'a');
+        dict.put('e', 'e');
+        dict.put('i', 'i');
+        dict.put('o', 'o');
+        dict.put('u', 'u');
+        dict.put('A', 'A');
+        dict.put('E', 'E');
+        dict.put('I', 'I');
+        dict.put('O', 'O');
+        dict.put('U', 'U');
+
+        StringBuilder sb = new StringBuilder(s);
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (dict.get(s.charAt(l)) != null && dict.get(s.charAt(r)) != null) {
+                sb.setCharAt(l, s.charAt(r)); 
+                sb.setCharAt(r, s.charAt(l)); 
+                l++;
+                r--;
+            } else if (dict.get(s.charAt(l)) == null && dict.get(s.charAt(r)) != null) {
+                l++;
+            } else if (dict.get(s.charAt(l)) != null && dict.get(s.charAt(r)) == null) {
+                r--;
+            } else {
+                l++;
+                r--;
+            }
+        } 
+
+        return sb.toString();
+    }
+}
