37f130059927cbf7c42dee27fabeabbccde38c39
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 22:55:44 +0200

    Create file

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 20/05/2025 22:55:43
+class Solution {
+    public int missingNumber(int[] nums) {
+        int[] t = new int[nums.length + 1];
+        for (int num : nums) {
+            t[num] = 1;
+        }
+        
+        for (int i = 0; i < t.length; i++) {
+            if (t[i] == 0) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
