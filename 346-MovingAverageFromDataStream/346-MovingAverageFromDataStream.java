f09f761e30a25fe4122b6daff5d37f0cdfadba3f
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:23:41 +0200

    Create file

---
+++ 346-MovingAverageFromDataStream/346-MovingAverageFromDataStream.java
@@ -0,0 +1,26 @@
+// Last updated: 20/05/2025 23:23:41
+class MovingAverage {
+    int size;
+    List queue = new ArrayList<>();
+
+    public MovingAverage(int size) {
+        this.size = size;
+    }
+    
+    public double next(int val) {
+        queue.add(val);
+        int res = 0;
+        
+        for (int i = Math.max(0, queue.size() - size); i < queue.size(); i++) {
+            res += (int) queue.get(i);
+        }
+        
+        return res * 1.0 / Math.min(queue.size(), size);
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
