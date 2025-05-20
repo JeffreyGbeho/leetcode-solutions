6f266f29f40f500595a04bc1bbc85ba503b28531
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 22:55:03 +0200

    Create file

---
+++ 1798-MaxNumberOfKSumPairs/1798-MaxNumberOfKSumPairs.java
@@ -0,0 +1,24 @@
+// Last updated: 20/05/2025 22:55:03
+import java.util.Arrays;
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int count = 0;
+        int i = 0;
+        int j = nums.length-1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] == k) {
+                i++;
+                j--;
+                count++;
+            } else if (k - nums[i] > nums[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return count;
+    }
+}
