bd8abf1441fabc6139450a7ea5fcba42f5494888
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:39:43 +0200

    Create file

---
+++ 908-MiddleOfTheLinkedList/908-MiddleOfTheLinkedList.java
@@ -0,0 +1,38 @@
+// Last updated: 20/05/2025 23:39:43
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode current = head;
+        int total = 0;
+
+        while (current != null) {
+            total++;
+            current = current.next;
+        }
+
+        int middle = total / 2;
+        System.out.println(middle);
+
+        current = head;
+        total = 0;
+
+        while (current != null) {
+            if (total == middle) {
+                break;
+            }
+            total++;
+            current = current.next;
+        }
+
+        return current;
+    }
+}
