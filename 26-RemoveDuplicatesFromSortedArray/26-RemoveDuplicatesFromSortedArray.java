34ccba5e80f5e71e9588a99dcad2cb779cee6765
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:42:47 +0200

    Create file

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+// Last updated: 20/05/2025 23:42:47
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int insertIndex = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] != nums[i]) {
+                nums[insertIndex] = nums[i];     
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
