2ff9d04464ed3caf637031d7e76120181a26de42
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:42:34 +0200

    Create file

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,23 @@
+// Last updated: 20/05/2025 23:42:34
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> ransomMap = new HashMap<>();
+        Map<Character, Integer> magazineMap = new HashMap<>();
+        
+        for (int i = 0; i < ransomNote.length(); i++) {
+            ransomMap.put(ransomNote.charAt(i), ransomMap.getOrDefault(ransomNote.charAt(i), 0) + 1);
+        }
+        
+        for (int i = 0; i < magazine.length(); i++) {
+            magazineMap.put(magazine.charAt(i), magazineMap.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+        
+        for (char letter : ransomMap.keySet()) {
+            if (!magazineMap.containsKey(letter) || magazineMap.get(letter) < ransomMap.get(letter)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
