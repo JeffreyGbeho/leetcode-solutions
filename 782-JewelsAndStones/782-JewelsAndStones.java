168537b647c27b620c12f29e25bb15fece7a985f
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 22:58:58 +0200

    Create file

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,16 @@
+// Last updated: 20/05/2025 22:58:58
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < stones.length(); i++) {
+            map.put(stones.charAt(i), map.getOrDefault(stones.charAt(i), 0) + 1);
+        }
+        
+        int res = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            res += map.getOrDefault(jewels.charAt(i), 0);
+        }
+        
+        return res;
+    }
+}
