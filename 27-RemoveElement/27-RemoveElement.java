f6e65868f9a33fe60f40aeb2998e362fa781af96
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:33:44 +0200

    Create file

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,17 @@
+// Last updated: 20/05/2025 23:33:44
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+}
