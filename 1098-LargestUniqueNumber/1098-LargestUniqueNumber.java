5b72c73fb92388bfd8124b82bfe0c1d994f606cf
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-20 23:33:12 +0200

    Create file

---
+++ 1098-LargestUniqueNumber/1098-LargestUniqueNumber.java
@@ -0,0 +1,19 @@
+// Last updated: 20/05/2025 23:33:12
+class Solution {
+    public int largestUniqueNumber(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        int max = -1;
+        
+        for (int index : count.keySet()) {
+            if (count.get(index) == 1) {
+                max = Math.max(max, index);
+            }
+        }
+        return max;
+    }
+}
