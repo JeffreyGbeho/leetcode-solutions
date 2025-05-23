fa748d07722ede5765ea56d188bc9a1cd7dc67c1
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:47:22 +0200

    Create 1940-MaximumXorForEachQuery

---
+++ 1940-MaximumXorForEachQuery/1940-MaximumXorForEachQuery.py
@@ -0,0 +1,17 @@
+# Last updated: 23/05/2025 19:47:22
+class Solution(object):
+    def getMaximumXor(self, nums, maximumBit):
+        n = len(nums)
+        ans = [0] * n
+        val = (1 << maximumBit) - 1  # Equivalent to 2^maximumBit - 1
+        
+        curr = 0
+        for num in nums:
+            curr ^= num
+
+        for i in range(n):
+            ans[i] = curr ^ val
+            curr ^= nums[n - 1 - i]
+
+        return ans
+        
