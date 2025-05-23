0b4d9744b2e780741f39df6378594266874d04e4
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:36:40 +0200

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,23 @@
+# Last updated: 23/05/2025 19:36:40
+class Solution(object):
+    def isPalindrome(self, x):
+        # if negative return false always
+        if x < 0:
+            return False
+
+        # array to store digits of the number
+        a = []
+
+        # fill the array
+        while x > 0:
+            a.append(x%10)
+            x //= 10
+        
+        n = len(a)
+
+        # check for palindrome
+        for i in range(n//2):
+            if a[i] != a[n - i - 1]:
+                return False
+        
+        return True
