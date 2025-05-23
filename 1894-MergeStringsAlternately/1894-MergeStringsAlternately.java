224c43ec1295be025f7388b0f73c267c3ece87d3
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:47:23 +0200

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.java
@@ -0,0 +1,20 @@
+// Last updated: 23/05/2025 19:47:23
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int l1 = word1.length();
+        int l2 = word2.length();
+        int maxL = l1 >= l2 ? l1 : l2;
+        String res = "";
+
+        for (int i = 0; i < maxL; i++) {
+                if (i < l1) {
+                    res += word1.charAt(i);
+                }
+                if (i < l2) {
+                    res += word2.charAt(i);
+                }
+        }
+
+        return res;
+    }
+}
