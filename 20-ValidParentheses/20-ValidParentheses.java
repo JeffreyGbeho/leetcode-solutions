f97641e428415f81a8d65e6d187937f86e58440f
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:25:03 +0200

    Create file

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,17 @@
+// Last updated: 23/05/2025 19:25:03
+class Solution {
+    public boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                // If the string becomes empty, it indicates all brackets are matched.
+                return s.isEmpty();
+            }
+        }
+    }
+}
