31883bf03e943e00da253294a75625459f822730
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 18:47:36 +0200

    Create file

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -0,0 +1,15 @@
+// Last updated: 23/05/2025 18:47:37
+class Solution {
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                               (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+}
