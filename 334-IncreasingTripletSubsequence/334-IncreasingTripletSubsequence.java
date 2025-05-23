60db42c96cb3aeed16da9c80e001303d2a59bbda
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:03:38 +0200

    Create file

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.java
@@ -0,0 +1,19 @@
+// Last updated: 23/05/2025 19:03:38
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int l = Integer.MAX_VALUE;
+        int m = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n > m) {
+                return true;
+            } else if (n < l) {
+                l = n;
+            } else if (n > l && n < m) {
+                m = n;
+            }
+        }
+
+        return false;
+    }
+}
