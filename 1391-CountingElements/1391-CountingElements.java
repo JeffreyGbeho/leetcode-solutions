bd86f667b1dcfd418a2bc79933cdfd0ba7275fed
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:23:36 +0200

    Create file

---
+++ 1391-CountingElements/1391-CountingElements.java
@@ -0,0 +1,16 @@
+// Last updated: 23/05/2025 19:23:37
+class Solution {
+    public int countElements(int[] arr) {
+        int total = 0;
+        Set<Integer> mySet = new HashSet<>();
+        for (int num : arr) {
+            mySet.add(num);
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            total += mySet.contains(arr[i] + 1) ? 1 : 0;
+        }
+        
+        return total;
+    }
+}
