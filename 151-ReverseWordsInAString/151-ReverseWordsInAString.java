d8202c69a229ca2f6111107eb58018d9461dfd70
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:35:22 +0200

    Create file

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,13 @@
+// Last updated: 23/05/2025 19:35:22
+class Solution {
+    public String reverseWords(String s) {
+        String[] splt = s.split(" +");
+        String res = "";
+
+        for (int i = splt.length-1; i >= 0; i--) {
+            res += " " + splt[i];
+        }
+
+        return res.trim();
+    }
+}
