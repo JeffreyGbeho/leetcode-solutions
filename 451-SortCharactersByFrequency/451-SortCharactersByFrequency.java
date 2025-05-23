ffd604e29eb32e1d8e7d0747463d5dd7a954f402
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 18:47:02 +0200

    Create file

---
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -0,0 +1,21 @@
+// Last updated: 23/05/2025 18:47:02
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        List<Character> characters = new ArrayList<>(map.keySet());        
+        Collections.sort(characters, (a, b) -> map.get(b) - map.get(a));
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : characters) {
+            int copies = map.get(c);
+            for (int i = 0; i < copies; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
