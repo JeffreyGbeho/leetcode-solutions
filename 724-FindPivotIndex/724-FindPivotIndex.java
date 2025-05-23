4c381ee965fd734a9a1ad5dda4ce77d34e581c74
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:24:01 +0200

    Create file

---
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,12 @@
+// Last updated: 23/05/2025 19:24:02
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+}
