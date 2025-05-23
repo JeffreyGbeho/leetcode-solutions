c50ab50992cf325b6c310a811de700831d3c5814
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:56:27 +0200

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.java
@@ -0,0 +1,33 @@
+// Last updated: 23/05/2025 19:56:27
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int l = nums.length;
+        int[] res = new int[l];
+        int t = 1;
+        int zero = 0;
+
+        for (int n : nums) {
+            if (n != 0) {
+                t *= n;
+            } else {
+                zero++;
+            }
+        }
+
+        for (int i = 0; i < l; i++) {
+            if (zero > 1) {
+                res[i] = 0;
+            } else if (zero == 1) {
+                if (nums[i] == 0) {
+                    res[i] = t;
+                } else {
+                    res[i] = 0;
+                }
+            } else {
+                res[i] = t / nums[i];
+            }
+        }
+
+        return res;
+    }
+}
