d8d6ea43908c605d3a31bcadf87a7d6097ec26e6
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:32:22 +0200

    Create file

---
+++ 1833-FindTheHighestAltitude/1833-FindTheHighestAltitude.java
@@ -0,0 +1,13 @@
+// Last updated: 23/05/2025 19:32:22
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int res = 0;
+        int sum = 0;
+        for (int i = 0; i < gain.length; i++) {
+            sum += gain[i];
+            res = Math.max(res, sum);
+        }
+
+        return res;
+    }
+}
