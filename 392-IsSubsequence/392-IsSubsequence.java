e7c954509766f8e4101b0c22ba8221295c06d0b0
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 19:24:16 +0200

    Create file

---
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -0,0 +1,16 @@
+// Last updated: 23/05/2025 19:24:16
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int l = 0;
+        int r = 0;
+
+        while (r < s.length() && l < t.length()) {
+            if (t.charAt(l) == s.charAt(r)) {
+                r++;
+            }
+            l++;
+        }
+
+        return r >= s.length();
+    }
+}
