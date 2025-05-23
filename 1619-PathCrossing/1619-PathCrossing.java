8a70dacf2b04ae6cc23b192aca6d374d241d0e6b
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-05-23 18:46:34 +0200

    Create file

---
+++ 1619-PathCrossing/1619-PathCrossing.java
@@ -0,0 +1,32 @@
+// Last updated: 23/05/2025 18:46:35
+class Solution {
+    public boolean isPathCrossing(String path) {
+        int x = 0;
+        int y = 0;
+        Set<Pair<Integer, Integer>> set = new HashSet<>();
+        set.add(new Pair<Integer, Integer>(x, y));
+
+        for (char letter : path.toCharArray()) {
+            if (letter == 'N') {
+                y += 1;
+            } else if (letter == 'S') {
+                y -= 1;
+            } else if (letter == 'E') {
+                x += 1;
+            } else if (letter == 'W') {
+                x -= 1;
+            }
+
+            Pair<Integer, Integer> current = new Pair<Integer, Integer>(x, y);
+
+            if (set.contains(current)) {
+                return true;
+            }
+
+            set.add(current);
+        }
+
+        return false;
+        
+    }
+}
