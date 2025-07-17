02621475ea2f1e9906610ca240113162f3b75d3c
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-07-17 18:34:09 +0200

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/version/java/605-CanPlaceFlowers_20250717_183409.java
@@ -0,0 +1,25 @@
+// Last updated: 17/07/2025 18:34:09
+/*
+ * O(n)
+ * 
+ * count the number of possibilities and compare it with the parameter
+*/
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int c = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean l = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean r = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                
+                if (l && r) {
+                    flowerbed[i] = 1;
+                    c++;
+                }
+            }
+        }
+
+        return c >= n;
+    }
+}
