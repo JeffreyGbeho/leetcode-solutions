e11c8fce53f14899c605de697425cc1fe21f8c63
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-07-20 10:48:50 +0200

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/java/283-MoveZeroes_20250720_104854.java
@@ -0,0 +1,24 @@
+// Last updated: 20/07/2025 10:48:54
+// O(n)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 1;
+
+        while (i < nums.length && j < nums.length && i < j) {
+            while (i < nums.length && nums[i] != 0) {
+                i++;
+            }
+
+            j = i + 1;
+            while (j < nums.length && nums[j] == 0) {
+                j++;
+            }
+
+            if (i < nums.length && j < nums.length && i < j) {
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+}
