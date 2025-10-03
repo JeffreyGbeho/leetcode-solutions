3fecf7ec822d888d342fe0b9af4c377e0096b5af
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-10-03 19:43:01 +0200

    Create 2128-ReversePrefixOfWord

---
+++ 2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.java
@@ -0,0 +1,25 @@
+// Last updated: 03/10/2025 19:43:01
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        char[] arr = word.toCharArray();
+        int right = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == ch) {
+                right = i;
+                break;
+            }
+        }
+
+        int left = 0;
+        while (left < right) {
+            char temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
