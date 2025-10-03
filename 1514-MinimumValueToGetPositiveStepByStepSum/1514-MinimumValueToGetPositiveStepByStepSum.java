f6172268f544058df601771441fbde2ca9afaa4e
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-10-03 19:43:14 +0200

    Create 1514-MinimumValueToGetPositiveStepByStepSum

---
+++ 1514-MinimumValueToGetPositiveStepByStepSum/1514-MinimumValueToGetPositiveStepByStepSum.java
@@ -0,0 +1,31 @@
+// Last updated: 03/10/2025 19:43:14
+class Solution {
+    public int minStartValue(int[] nums) {
+        // int[] prefix = new int[nums.length];
+        
+        // prefix[0] = nums[0];
+        // for (int i = 1; i < nums.length; i++) {
+        //     prefix[i] = prefix[i-1] + nums[i];
+        // }
+        
+        // Arrays.sort(prefix);
+        
+        // if (prefix[0] <= 0) {
+        //     return Math.abs(prefix[0]) + 1;
+        // } else {
+        //     return 1;
+        // }
+        int minValue = 0;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+            minValue = Math.min(minValue, total);
+        }
+
+        if (minValue <= 0) {
+            return Math.abs(minValue) + 1;
+        } else {
+            return 1;
+        }
+    }
+}
