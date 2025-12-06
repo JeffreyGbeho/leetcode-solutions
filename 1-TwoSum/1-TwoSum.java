fda3c160b80ba0ba2581eaecad39b28ba468418d
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-12-06 16:37:22 +0100

    File updated at 06/12/2025 10:37:22

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,20 +1,16 @@
-// Last updated: 03/10/2025 19:44:19
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            // Check if complement exists → return indices if found
-            var temp = map.get(target - nums[i]);
-            if (temp != null) {
-                int[] res = {temp, i};
-                return res;
-            }
-
-            // Store current number → index mapping
-            map.put(nums[i], i);
-        }
-
-        return null;
-    }
-}
+// Last updated: 06/12/2025 10:37:22
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        for (int i = 0; i < nums.length; i++) {
+4            int t = target-nums[i];
+5            for (int j = i+1; j < nums.length; j++) {
+6                if (nums[j] == t) {
+7                    int[] res = {i, j};
+8                    return res;
+9                }
+10            }
+11        }
+12
+13        return new int[2];
+14    }
+15}
