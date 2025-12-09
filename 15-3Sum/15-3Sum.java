a7939b1ff75f48cca75e9ed485d70481a485e30a
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-12-10 00:58:03 +0100

    File updated at 09/12/2025 18:58:03

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,36 +1,49 @@
-// Last updated: 29/11/2025 10:43:55
+// Last updated: 09/12/2025 18:58:03
 1class Solution {
 2    public List<List<Integer>> threeSum(int[] nums) {
-3         List<List<Integer>> res = new ArrayList<>();
-4        if (nums.length < 3) return res;
-5
-6        Arrays.sort(nums);// Sort array
+3         List<List<Integer>> ans = new ArrayList<>();
+4
+5        // Sort the array
+6        Arrays.sort(nums);
 7
 8        for (int i = 0; i < nums.length - 2; i++) {
-9            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
-10            if (nums[i] > 0) break; 
-11
-12            int left = i + 1, right = nums.length - 1;
-13            int target = -nums[i];
-14
-15            while (left < right) {
-16                int sum = nums[left] + nums[right];
-17                if (sum == target) {
-18                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-19                    left++;
-20                    right--;
-21
-22                    // skip duplicates
-23                    while (left < right && nums[left] == nums[left - 1]) left++;
-24                    while (left < right && nums[right] == nums[right + 1]) right--;
-25                } else if (sum < target) {
-26                    left++;
-27                } else {
-28                    right--;
-29                }
-30            }
-31        }
-32
-33        return res;
-34    }
-35}
+9            // Skip duplicate elements for i
+10            if (i > 0 && nums[i] == nums[i - 1]) {
+11                continue;
+12            }
+13
+14            int j = i + 1;
+15            int k = nums.length - 1;
+16
+17            while (j < k) {
+18                int sum = nums[i] + nums[j] + nums[k];
+19
+20                if (sum == 0) {
+21                    // Found a triplet with zero sum
+22                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+23
+24                    // Skip duplicate elements for j
+25                    while (j < k && nums[j] == nums[j + 1]) {
+26                        j++;
+27                    }
+28
+29                    // Skip duplicate elements for k
+30                    while (j < k && nums[k] == nums[k - 1]) {
+31                        k--;
+32                    }
+33
+34                    // Move the pointers
+35                    j++;
+36                    k--;
+37                } else if (sum < 0) {
+38                    // Sum is less than zero, increment j to increase the sum
+39                    j++;
+40                } else {
+41                    // Sum is greater than zero, decrement k to decrease the sum
+42                    k--;
+43                }
+44            }
+45        }
+46        return ans;
+47    }
+48}
