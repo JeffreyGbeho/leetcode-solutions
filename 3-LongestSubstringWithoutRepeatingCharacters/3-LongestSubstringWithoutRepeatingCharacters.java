edb7d89dd2a6ed41351f741d29bb89a41d39e786
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2025-12-11 00:29:58 +0100

    File updated at 10/12/2025 18:29:58

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,20 +1,21 @@
-// Last updated: 03/10/2025 19:44:16
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        int[] charIndex = new int[128];
-        Arrays.fill(charIndex, -1);
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s.charAt(right)] >= left) {
-                left = charIndex[s.charAt(right)] + 1;
-            }
-            charIndex[s.charAt(right)] = right;
-            maxLength = Math.max(maxLength, right - left + 1);
-        }
-        
-        return maxLength;
-    }
-}
+// Last updated: 10/12/2025 18:29:58
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        StringBuilder sb = new StringBuilder();
+4        int res = 0;
+5
+6        for (int i = 0; i < s.length(); i++) {
+7            if (sb.toString().indexOf(s.charAt(i)) == -1) {
+8                sb.append(s.charAt(i));
+9                res = Math.max(res, sb.toString().length());
+10            } else {
+11                while (sb.toString().indexOf(s.charAt(i)) != -1) {
+12                    sb = sb.deleteCharAt(0);
+13                }
+14                sb.append(s.charAt(i));
+15            }
+16        }
+17
+18        return res;
+19    }
+20}
