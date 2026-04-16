cfb04188dc628eb831131c092a88d073e902552f
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-16 18:02:33 -0400

    File updated at 16/04/2026, 18:02:33

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,30 +1,20 @@
-// Last updated: 06/12/2025 10:45:46
+// Last updated: 16/04/2026, 18:02:33
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3        // for (int i = 0; i < nums.length; i++) {
-4        //     int t = target-nums[i];
-5        //     for (int j = i+1; j < nums.length; j++) {
-6        //         if (nums[j] == t) {
-7        //             int[] res = {i, j};
-8        //             return res;
-9        //         }
-10        //     }
-11        // }
-12
-13        // return new int[2];
-14
-15        Map<Integer, Integer> sto = new HashMap<>();
+3        Map<Integer, Integer> diff = new HashMap<>();
+4        int[] res = new int[2];
+5        for (int i = 0; i < nums.length; i++) {
+6            if (diff.get(nums[i]) != null) {
+7                res[0] = diff.get(nums[i]);
+8                res[1] = i;
+9                return res;
+10            }
+11
+12            if (diff.get(target - nums[i]) == null) {
+13                diff.put(target - nums[i], i);
+14            }
+15        }
 16
-17        for (int i = 0; i < nums.length; i++) {
-18            if (sto.containsKey(nums[i])) {
-19                int[] res = {sto.get(nums[i]), i};
-20                return res;
-21            }
-22
-23            int sum = target-nums[i];
-24            sto.put(sum, i);
-25        }
-26
-27        return new int[2];
-28    }
-29}
+17        return res;
+18    }
+19}
