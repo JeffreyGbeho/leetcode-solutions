22be39212e048d386a4ce3a86ba3927debd038e7
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-17 15:09:06 -0400

    File updated at 17/04/2026, 15:09:06

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,16 +1,25 @@
-// Last updated: 08/12/2025 14:00:41
+// Last updated: 17/04/2026, 15:09:06
 1class Solution {
 2    public boolean isPalindrome(String s) {
-3        StringBuilder sb = new StringBuilder();
-4        for (Character c : s.toCharArray()) {
-5            if (Character.isAlphabetic(c) || Character.isDigit(c)) {
-6                sb.append(c);
-7            }
-8        }
-9
-10        String original = sb.toString().toLowerCase();
-11        String reverse = sb.reverse().toString().toLowerCase();
-12
-13        return original.equals(reverse);
-14    }
-15}
+3        int i = 0;
+4        int j = s.length() - 1;
+5
+6        while (i < j) {
+7            while (i < s.length() && (!Character.isAlphabetic(s.charAt(i)) && !Character.isDigit(s.charAt(i)))) {
+8                i++;
+9            }
+10            while (j >= 0 && (!Character.isAlphabetic(s.charAt(j)) && !Character.isDigit(s.charAt(j)))) {
+11                j--;
+12            }
+13
+14            if (i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+15                return false;
+16            }
+17
+18            i++;
+19            j--;
+20        }
+21
+22        return true;
+23    }
+24}
