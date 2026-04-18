09734724b9b9025cfadd64377f4020562a91fea6
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-18 11:05:04 -0400

    File updated at 18/04/2026, 11:05:04

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,49 +1,40 @@
-// Last updated: 09/12/2025 18:58:03
+// Last updated: 18/04/2026, 11:05:04
 1class Solution {
 2    public List<List<Integer>> threeSum(int[] nums) {
-3         List<List<Integer>> ans = new ArrayList<>();
+3        List<List<Integer>> res = new ArrayList<>();
 4
-5        // Sort the array
-6        Arrays.sort(nums);
-7
-8        for (int i = 0; i < nums.length - 2; i++) {
-9            // Skip duplicate elements for i
-10            if (i > 0 && nums[i] == nums[i - 1]) {
-11                continue;
-12            }
-13
-14            int j = i + 1;
-15            int k = nums.length - 1;
-16
-17            while (j < k) {
-18                int sum = nums[i] + nums[j] + nums[k];
-19
-20                if (sum == 0) {
-21                    // Found a triplet with zero sum
-22                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-23
-24                    // Skip duplicate elements for j
-25                    while (j < k && nums[j] == nums[j + 1]) {
-26                        j++;
-27                    }
-28
-29                    // Skip duplicate elements for k
-30                    while (j < k && nums[k] == nums[k - 1]) {
-31                        k--;
-32                    }
-33
-34                    // Move the pointers
-35                    j++;
-36                    k--;
-37                } else if (sum < 0) {
-38                    // Sum is less than zero, increment j to increase the sum
-39                    j++;
-40                } else {
-41                    // Sum is greater than zero, decrement k to decrease the sum
-42                    k--;
-43                }
-44            }
-45        }
-46        return ans;
-47    }
-48}
+5        Arrays.sort(nums); // O(n log n)
+6
+7        for (int i = 0; i < nums.length - 2; i++) { // O(n²)
+8            if (i > 0 && nums[i] == nums[i-1]) continue;
+9
+10            int target = 0 - nums[i];
+11
+12            int left = i + 1;
+13            int right = nums.length - 1;
+14
+15            while (left < right) {
+16                if (left > i+1 && nums[left] == nums[left-1]) {
+17                    left++;
+18                    continue;
+19                }
+20                if (right < nums.length - 1 && nums[right] == nums[right+1]) {
+21                    right--;
+22                    continue;
+23                }
+24
+25                if (target == nums[left] + nums[right]) {
+26                    res.add(Arrays.asList(nums[i],nums[left], nums[right]));
+27                    right--;
+28                    left++;
+29                } else if (target < nums[left] + nums[right]) {
+30                    right--;
+31                } else {
+32                    left++;
+33                }
+34            }
+35        }
+36
+37        return res;
+38    }
+39}
