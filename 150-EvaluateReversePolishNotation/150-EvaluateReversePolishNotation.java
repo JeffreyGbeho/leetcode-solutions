388aa1704d3ec480979fdf7853ab2f8b96f32222
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-20 19:34:38 -0400

    File updated at 20/04/2026, 19:34:38

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,39 +1,37 @@
-// Last updated: 20/04/2026, 19:28:36
+// Last updated: 20/04/2026, 19:34:38
 1class Solution {
-2    public int evalRPN(String[] tokens) {
-3        Deque<Integer> stack = new ArrayDeque<>();
-4
+2    Deque<Integer> stack = new ArrayDeque<>();
+3
+4    public int evalRPN(String[] tokens) {
 5        for (String token : tokens) {
 6            Integer v = null;
 7            if (token.equals("+")) {
-8                int b = stack.getLast();
-9                stack.removeLast();
-10                int a = stack.getLast();
-11                stack.removeLast();
-12                v = a + b;
-13            } else if (token.equals("*")) {
-14                int b = stack.getLast();
-15                stack.removeLast();
-16                int a = stack.getLast();
-17                stack.removeLast();
-18                v = a * b;
-19            } else if (token.equals("-")) {
-20                int b = stack.getLast();
-21                stack.removeLast();
-22                int a = stack.getLast();
-23                stack.removeLast();
-24                v = a - b;
-25            } else if (token.equals("/")) {
-26                int b = stack.getLast();
-27                stack.removeLast();
-28                int a = stack.getLast();
-29                stack.removeLast();
-30                v = a / b;
-31            }
-32
-33            stack.addLast(v != null ? v : Integer.parseInt(token));
-34        }
-35
-36        return stack.getLast();
-37    }
-38}
+8                int[] val = extractValues();
+9                v = val[0] + val[1];
+10            } else if (token.equals("*")) {
+11                int[] val = extractValues();
+12                v = val[0] * val[1];
+13            } else if (token.equals("-")) {
+14                int[] val = extractValues();
+15                v = val[0] - val[1];
+16            } else if (token.equals("/")) {
+17                int[] val = extractValues();
+18                v = val[0] / val[1];
+19            }
+20
+21            stack.addLast(v != null ? v : Integer.parseInt(token));
+22        }
+23
+24        return stack.getLast();
+25    }
+26
+27    private int[] extractValues() {
+28        int[] r = new int[2];
+29        r[1] = stack.getLast();
+30        stack.removeLast();
+31        r[0] = stack.getLast();
+32        stack.removeLast();
+33        
+34        return r;
+35    }
+36}
