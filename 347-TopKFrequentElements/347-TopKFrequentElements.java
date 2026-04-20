f14c9ea78644a47e02270a8b43dea19136fa7368
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-20 13:07:52 -0400

    File updated at 20/04/2026, 13:07:52

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,29 +1,32 @@
-// Last updated: 06/12/2025 18:04:26
+// Last updated: 20/04/2026, 13:07:52
 1class Solution {
 2    public int[] topKFrequent(int[] nums, int k) {
-3        Map<Integer, Integer> occ = new HashMap<>();
-4
-5        for (Integer num : nums) {
-6            occ.put(num, occ.getOrDefault(num, 0) + 1);
-7        }
-8
-9        HashMap<Integer, Integer> temp = occ.entrySet().stream().sorted((i1, i2)-> 
-10            i2.getValue().compareTo(i1.getValue())).collect(
-11            Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,
-12            (e1, e2) -> e1, LinkedHashMap::new));
-13
-14        int[] res = new int[k];
+3        Map<Integer, Integer> map = new HashMap<>();
+4        for (int num : nums) {
+5            map.put(num, map.getOrDefault(num, 0) + 1);
+6        }
+7
+8        List<Integer>[] bs = new List[nums.length + 1];
+9        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+10            if (bs[entry.getValue()] == null) {
+11                bs[entry.getValue()] = new ArrayList<>(); 
+12            }
+13            bs[entry.getValue()].add(entry.getKey());
+14        }
 15
-16        int j = 0;
-17        for (Integer key : temp.keySet()) {
-18            res[j] = key;
-19            j++;
-20
-21            if (j >= k) {
-22                break;
-23            }
-24        }
-25
-26        return res;
-27    }
-28}
+16        int[] res = new int[k];
+17        int index = 0;
+18        for (int i = bs.length - 1; i >= 0 && index < k; i--) {
+19            if (bs[i] != null) {
+20                int j = 0;
+21                while (j < bs[i].size()) {
+22                    res[index] = bs[i].get(j);
+23                    index++;
+24                    j++;
+25                }
+26            }
+27        }
+28
+29        return res;
+30    }
+31}
