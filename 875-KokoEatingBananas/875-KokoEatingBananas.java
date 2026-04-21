ae2642ae9bca027763db153ef60107d3486e0e9d
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-21 15:28:54 -0400

    File updated at 21/04/2026, 15:28:54

--- 875-KokoEatingBananas/875-KokoEatingBananas.java
+++ 875-KokoEatingBananas/875-KokoEatingBananas.java
@@ -1,30 +1,28 @@
-// Last updated: 21/04/2026, 15:28:31
+// Last updated: 21/04/2026, 15:28:54
 1class Solution {
 2    public int minEatingSpeed(int[] piles, int h) {
-3        Arrays.sort(piles);
-4
-5        int min = 1;
-6        int max = 0;
-7        for (int pile : piles) {
-8            max = Math.max(max, pile);
-9        }
-10
-11
-12        while (min < max) {
-13            int mid = (max + min) / 2;
-14
-15            int sum = 0;
-16            for (int pile : piles) {
-17                sum += Math.ceil((double) pile / mid);;
-18            }
-19
-20            if (sum <= h) {
-21                max = mid;
-22            } else {
-23                min = mid + 1;
-24            }
-25        }
-26
-27        return min;
-28    }
-29}
+3        int min = 1;
+4        int max = 0;
+5        for (int pile : piles) {
+6            max = Math.max(max, pile);
+7        }
+8
+9
+10        while (min < max) {
+11            int mid = (max + min) / 2;
+12
+13            int sum = 0;
+14            for (int pile : piles) {
+15                sum += Math.ceil((double) pile / mid);;
+16            }
+17
+18            if (sum <= h) {
+19                max = mid;
+20            } else {
+21                min = mid + 1;
+22            }
+23        }
+24
+25        return min;
+26    }
+27}
