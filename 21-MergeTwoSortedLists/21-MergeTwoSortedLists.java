a55e0207c147139666ea3faf462020778de988f1
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-22 12:04:49 -0400

    File updated at 22/04/2026, 12:04:49

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-// Last updated: 22/04/2026, 12:02:07
+// Last updated: 22/04/2026, 12:04:49
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -12,41 +12,25 @@
 11class Solution {
 12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 13        ListNode dummy = new ListNode();
-14        ListNode prev = null;
+14        ListNode prev = dummy;
 15        while (list1 != null && list2 != null) {
 16            if (list1.val <= list2.val) {
-17                if (prev != null) {
-18                    prev.next = list1;
-19                }
-20                prev = list1;
-21                list1 = list1.next;
-22            } else {
-23                if (prev != null) {
-24                    prev.next = list2;
-25                }
-26                prev = list2;
-27                list2 = list2.next;
-28            }
-29
-30            if (dummy.next == null) {
-31                dummy.next = prev;
-32            }
-33        }
-34
-35        if (list1 != null) {
-36            if (prev == null) {
-37                dummy.next = list1;
-38            } else {
-39                prev.next = list1;
-40            }
-41        } else if (list2 != null) {
-42            if (prev == null) {
-43                dummy.next = list2;
-44            } else {
-45                prev.next = list2;
-46            }
-47        }
-48
-49        return dummy.next;
-50    }
-51}
+17                prev.next = list1;
+18                prev = list1;
+19                list1 = list1.next;
+20            } else {
+21                prev.next = list2;
+22                prev = list2;
+23                list2 = list2.next;
+24            }
+25        }
+26
+27        if (list1 != null) {
+28            prev.next = list1;
+29        } else if (list2 != null) {
+30            prev.next = list2;
+31        }
+32
+33        return dummy.next;
+34    }
+35}
