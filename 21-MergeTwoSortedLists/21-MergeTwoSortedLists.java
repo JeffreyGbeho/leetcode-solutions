001c9ac34b4829ff95bf07ad5696c00ef0173316
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-04-22 12:02:07 -0400

    File updated at 22/04/2026, 12:02:07

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,54 +1,52 @@
-// Last updated: 03/10/2025 19:44:02
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null){return null;}
-        if(list1 == null && list2 != null){return list2;}
-        if(list1 != null && list2 == null){return list1;}
-        ListNode i = list1,j=list2;
-        ListNode head = null,tail=null;
-        while((i!=null) && (j!=null)){
-            if(i.val<=j.val){
-                if(head == null){
-                    head = i;
-                    tail = i;
-                    i  = i.next;
-                }else{
-                    tail.next = i;
-                    tail = tail.next;
-                    i = i.next;
-                }
-            }else{
-                if(head == null){
-                    head = j;
-                    tail = j;
-                    j  = j.next;
-                }else{
-                    tail.next = j;
-                    tail = tail.next;
-                    j = j.next;
-                }
-            }
-        }
-        while(i!=null){
-            tail.next=i;
-            tail = tail.next;
-            i = i.next;
-        }
-        while(j!=null){
-            tail.next=j;
-            tail = tail.next;
-            j = j.next;
-        }
-        return head;
-    }
-}
+// Last updated: 22/04/2026, 12:02:07
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+13        ListNode dummy = new ListNode();
+14        ListNode prev = null;
+15        while (list1 != null && list2 != null) {
+16            if (list1.val <= list2.val) {
+17                if (prev != null) {
+18                    prev.next = list1;
+19                }
+20                prev = list1;
+21                list1 = list1.next;
+22            } else {
+23                if (prev != null) {
+24                    prev.next = list2;
+25                }
+26                prev = list2;
+27                list2 = list2.next;
+28            }
+29
+30            if (dummy.next == null) {
+31                dummy.next = prev;
+32            }
+33        }
+34
+35        if (list1 != null) {
+36            if (prev == null) {
+37                dummy.next = list1;
+38            } else {
+39                prev.next = list1;
+40            }
+41        } else if (list2 != null) {
+42            if (prev == null) {
+43                dummy.next = list2;
+44            } else {
+45                prev.next = list2;
+46            }
+47        }
+48
+49        return dummy.next;
+50    }
+51}
