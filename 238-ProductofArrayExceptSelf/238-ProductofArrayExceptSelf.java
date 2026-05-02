fb52c381e9e83fcd129b742b284de1f760bf2021
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-02 10:07:43 -0400

    File updated at 02/05/2026, 10:07:43

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,19 +1,25 @@
-// Last updated: 07/12/2025 10:12:39
+// Last updated: 02/05/2026, 10:07:43
 1class Solution {
 2    public int[] productExceptSelf(int[] nums) {
-3        int n = nums.length;
-4        int ans[] = new int[n];
-5        Arrays.fill(ans, 1);
-6        int curr = 1;
-7        for(int i = 0; i < n; i++) {
-8            ans[i] *= curr;
-9            curr *= nums[i];
-10        }
-11        curr = 1;
-12        for(int i = n - 1; i >= 0; i--) {
-13            ans[i] *= curr;
-14            curr *= nums[i];
+3        int[] left = new int[nums.length];
+4        left[0] = 1;
+5        int[] right = new int[nums.length];
+6        right[nums.length - 1] = 1;
+7        int[] ans = new int[nums.length];
+8
+9        for (int i = 1; i < nums.length; i++) {
+10            left[i] = left[i-1] * nums[i-1];
+11        }
+12
+13        for (int i = nums.length - 2; i >= 0; i--) {
+14            right[i] = right[i+1] * nums[i+1];
 15        }
-16        return ans;
-17    }
-18}
+16
+17        for (int i = 0; i < left.length; i++) {
+18            ans[i] = left[i] * right[i];
+19        }
+20
+21        return ans;
+22
+23    }
+24}
