f306d9859836bf8a135142138f1a353df05a6177
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-02 16:20:58 -0400

    File updated at 02/05/2026, 16:20:58

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,23 +1,21 @@
-// Last updated: 02/05/2026, 16:20:10
+// Last updated: 02/05/2026, 16:20:58
 1class Solution {
 2    public int eraseOverlapIntervals(int[][] intervals) {
 3        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-4        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
-5        int ans = 0;
+4        int ans = 0;
+5        int end = intervals[0][1];
 6
-7        for (int i = 1; i < list.size(); i++) {
-8            if (list.get(i)[0] < list.get(i-1)[1]) {
-9                if (list.get(i)[1] < list.get(i-1)[1]) {
-10                    list.remove(i-1);
-11                } else {
-12                    list.remove(i);
-13                }
-14
-15                ans++;
-16                i--;
-17            }
-18        }
-19
-20        return ans;
-21    }
-22}
+7        for (int i = 1; i < intervals.length; i++) {
+8            if (intervals[i][0] < end) {
+9                // overlap → supprimer celui qui finit le plus tard
+10                ans++;
+11                end = Math.min(end, intervals[i][1]);
+12            } else {
+13                // pas d'overlap → avancer
+14                end = intervals[i][1];
+15            }
+16        }
+17
+18        return ans;
+19    }
+20}
