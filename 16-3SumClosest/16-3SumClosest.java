ba2112416762b7b7e859cbde1ff3581084132187
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-08 11:59:44 -0400

    File updated at 08/05/2026, 11:59:44

--- 16-3SumClosest/16-3SumClosest.java
+++ 16-3SumClosest/16-3SumClosest.java
@@ -1,26 +1,30 @@
-// Last updated: 29/11/2025 10:43:13
+// Last updated: 08/05/2026, 11:59:44
 1class Solution {
 2    public int threeSumClosest(int[] nums, int target) {
 3        Arrays.sort(nums);
-4        int closest_sum = Integer.MAX_VALUE / 2;  // A large value but not overflow
-5        
-6        for (int i = 0; i < nums.length - 2; ++i) {
-7            int left = i + 1, right = nums.length - 1;
-8            while (left < right) {
-9                int current_sum = nums[i] + nums[left] + nums[right];
-10                if (Math.abs(current_sum - target) < Math.abs(closest_sum - target)) {
-11                    closest_sum = current_sum;
-12                }
-13                if (current_sum < target) {
-14                    ++left;
-15                } else if (current_sum > target) {
-16                    --right;
-17                } else {
-18                    return current_sum;
-19                }
-20            }
-21        }
-22        
-23        return closest_sum;
-24    }
-25}
+4        int res = nums[0] + nums[1] + nums[2];
+5
+6        for (int i = 0; i < nums.length-2; i++) {
+7            int left = i+1;
+8            int right = nums.length -1;
+9
+10            while (left < right) {
+11                int sum = nums[i] + nums[left] + nums[right];
+12
+13                if (sum == target) {
+14                    return sum;
+15                }
+16
+17                res = Math.abs(sum - target) < Math.abs(res - target) ? sum : res;
+18
+19                if (sum > target) {
+20                    right--;
+21                } else {
+22                    left++;
+23                }
+24            }
+25        }
+26
+27        return res;
+28    }
+29}
