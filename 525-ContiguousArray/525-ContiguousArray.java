4e31031e2e1fa9955d8c8b2f46d5e7d151df6983
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-13 12:37:10 -0400

    File updated at 13/05/2026, 12:37:10

--- 525-ContiguousArray/525-ContiguousArray.java
+++ 525-ContiguousArray/525-ContiguousArray.java
@@ -1,17 +1,20 @@
-// Last updated: 03/10/2025 19:43:32
-class Solution {
-    public int findMaxLength(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        int maxlen = 0, count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            count = count + (nums[i] == 1 ? 1 : -1);
-            if (map.containsKey(count)) {
-                maxlen = Math.max(maxlen, i - map.get(count));
-            } else {
-                map.put(count, i);
-            }
-        }
-        return maxlen;
-    }
-}
+// Last updated: 13/05/2026, 12:37:10
+1class Solution {
+2    public int findMaxLength(int[] nums) {
+3        int res = 0;
+4        int c = 0;
+5        Map<Integer, Integer> map = new HashMap<>();
+6        map.put(0, -1);
+7        for (int i = 0; i < nums.length; i++) {
+8            c += nums[i] == 0 ? -1 : 1;
+9
+10            if (map.get(c) != null) {
+11                res = Math.max(res, i - map.get(c));
+12            } else {
+13                map.put(c, i);
+14            }
+15        }
+16
+17        return res;
+18    }
+19}
