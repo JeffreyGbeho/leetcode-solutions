2d7ee656c7c9371fa24520dddda39e44c0805f18
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-16 10:13:15 -0400

    File updated at 16/05/2026, 10:13:14

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,13 +1,28 @@
-// Last updated: 03/10/2025 19:43:57
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        // Solution O(n)
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target) {
-                return i;
-            }
-        }
-
-        return nums.length;
-    }
-}
+// Last updated: 16/05/2026, 10:13:14
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int left = 0;
+4        int mid = nums.length / 2;
+5        int right = nums.length - 1;
+6
+7        while (left <= right) {
+8            if (nums[mid] == target) {
+9                return mid;
+10            }
+11
+12            if (target < nums[mid]) {
+13                right = mid - 1;
+14            } else {
+15                left = mid + 1;
+16            }
+17
+18            mid = (left + right) / 2;
+19        }
+20
+21        if (nums[mid] < target) {
+22            return mid + 1;
+23        } else {
+24            return mid - 1 < 0 ? 0 : mid - 1;
+25        }
+26    }
+27}
