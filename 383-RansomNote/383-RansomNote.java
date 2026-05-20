a0a5a6e497b58456a66a90ea391eeee7a2eb9128
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-20 09:30:00 -0400

    File updated at 20/05/2026, 09:30:00

--- 383-RansomNote/383-RansomNote.java
+++ 383-RansomNote/383-RansomNote.java
@@ -1,23 +1,20 @@
-// Last updated: 03/10/2025 19:43:36
-class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> ransomMap = new HashMap<>();
-        Map<Character, Integer> magazineMap = new HashMap<>();
-        
-        for (int i = 0; i < ransomNote.length(); i++) {
-            ransomMap.put(ransomNote.charAt(i), ransomMap.getOrDefault(ransomNote.charAt(i), 0) + 1);
-        }
-        
-        for (int i = 0; i < magazine.length(); i++) {
-            magazineMap.put(magazine.charAt(i), magazineMap.getOrDefault(magazine.charAt(i), 0) + 1);
-        }
-        
-        for (char letter : ransomMap.keySet()) {
-            if (!magazineMap.containsKey(letter) || magazineMap.get(letter) < ransomMap.get(letter)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-}
+// Last updated: 20/05/2026, 09:30:00
+1class Solution {
+2    public boolean canConstruct(String ransomNote, String magazine) {
+3        Map<Character, Integer> rn = new HashMap<>();
+4        for (char c : ransomNote.toCharArray()) {
+5            rn.put(c, rn.getOrDefault(c, 0) + 1);
+6        }
+7
+8        Map<Character, Integer> mz = new HashMap<>();
+9        for (char c : magazine.toCharArray()) {
+10            mz.put(c, mz.getOrDefault(c, 0) + 1);
+11        }
+12
+13        for (char c : rn.keySet()) {
+14            if (mz.get(c) == null || mz.get(c) < rn.get(c)) return false;
+15        }
+16
+17        return true;
+18    }
+19}
