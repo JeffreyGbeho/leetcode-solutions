2b2f548f802bbce7022590dbcd78a0ca06f7cec2
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-21 11:13:51 -0400

    File updated at 21/05/2026, 11:13:51

--- 166-FractiontoRecurringDecimal/166-FractiontoRecurringDecimal.java
+++ 166-FractiontoRecurringDecimal/166-FractiontoRecurringDecimal.java
@@ -1,36 +1,37 @@
-// Last updated: 21/05/2026, 11:13:19
+// Last updated: 21/05/2026, 11:13:51
 1class Solution {
 2    public String fractionToDecimal(int numerator, int denominator) {
-3        Map<Long, Integer> rest = new HashMap<>();
-4
-5        long v = Math.abs((long) numerator) / Math.abs((long) denominator);
-6        long r = (long) numerator % (long) denominator;
-7
-8        if (numerator == 0) return "0";
+3        if (numerator == 0) return "0";
+4        
+5        Map<Long, Integer> rest = new HashMap<>();
+6
+7        long v = Math.abs((long) numerator) / Math.abs((long) denominator);
+8        long r = (long) numerator % (long) denominator;
 9
-10        if (r == 0) return ((numerator < 0) ^ (denominator < 0) ? "-" : "") + v;
-11
-12        rest.put(r, 0);
-13        StringBuilder sb = new StringBuilder();
-14        int i = 1;
-15
-16        while (r != 0) {
-17            r *= 10;
-18            long tmp = Math.abs((long) r) / Math.abs((long) denominator);
-19            r = r % denominator;
-20
-21            sb.append("" + tmp);
-22
-23            if (rest.get(r) != null) {
-24                sb.insert((int) rest.get(r), '(');
-25                sb.append(')');
-26                break;
-27            }
-28
-29            rest.put(r, i);
-30            i++;
-31        }
-32
-33        return ((numerator < 0) ^ (denominator < 0) ? "-" : "") + v + "." + sb.toString();
-34    }
-35}
+10
+11        if (r == 0) return ((numerator < 0) ^ (denominator < 0) ? "-" : "") + v;
+12
+13        rest.put(r, 0);
+14        StringBuilder sb = new StringBuilder();
+15        int i = 1;
+16
+17        while (r != 0) {
+18            r *= 10;
+19            long tmp = Math.abs((long) r) / Math.abs((long) denominator);
+20            r = r % denominator;
+21
+22            sb.append("" + tmp);
+23
+24            if (rest.get(r) != null) {
+25                sb.insert((int) rest.get(r), '(');
+26                sb.append(')');
+27                break;
+28            }
+29
+30            rest.put(r, i);
+31            i++;
+32        }
+33
+34        return ((numerator < 0) ^ (denominator < 0) ? "-" : "") + v + "." + sb.toString();
+35    }
+36}
