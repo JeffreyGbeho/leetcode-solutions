3a13d77847034f2c0d2079242063838311e5dc06
Jeffrey Gbeho <150350006+JeffreyGbeho@users.noreply.github.com>
2026-05-24 17:51:37 -0400

    File updated at 24/05/2026, 17:51:37

--- 71-SimplifyPath/71-SimplifyPath.java
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -1,27 +1,24 @@
-// Last updated: 03/10/2025 19:43:51
-class Solution {
-    public String simplifyPath(String path) {
-        Stack<String> s = new Stack<String>();
-        String[] items = path.split("/");
-
-        for (String str : items) {
-            if (str.equals(".") || str.isEmpty()) {
-                continue;
-            } else if (str.equals("..")) {
-                if (!s.isEmpty()) {
-                    s.pop();
-                }
-            } else {
-                s.add(str);
-            }
-        }
-
-        StringBuilder res = new StringBuilder();
-        for (String dir : s) {
-            res.append("/");
-            res.append(dir);
-        }
-
-        return res.length() > 0 ? res.toString() : "/";
-    }
-}
+// Last updated: 24/05/2026, 17:51:37
+1class Solution {
+2    public String simplifyPath(String path) {
+3        Deque<String> stack = new ArrayDeque<>();
+4
+5        String[] str = path.split("/");
+6
+7        for (String s : str) {
+8            if (s.equals("..")) {
+9                stack.poll();
+10            } else if (!s.isEmpty() && !s.equals(".")) {
+11                stack.push(s);
+12            }
+13        }
+14
+15        String res = "";
+16
+17        while (stack.size() > 0) {
+18            res += "/" + stack.pollLast();
+19        }
+20
+21        return res.isEmpty() ? "/" : res;
+22    }
+23}
